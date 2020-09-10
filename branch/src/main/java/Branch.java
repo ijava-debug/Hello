5f149ff44131e0463f167542ba2333d45e265895
ijava-debug <70931287+ijava-debug@users.noreply.github.com>
2020-09-10 09:44:53 +0800

    Update Branch.java

--- branch/src/main/java/Branch.java
+++ branch/src/main/java/Branch.java
@@ -4,5 +4,6 @@ public class Branch {
         System.out.println("主干修改的");
         System.out.println("主干修改了");
         System.out.println("分支修改了");
+        System.out.prinltn("远程仓库修改了");
     }
 }
